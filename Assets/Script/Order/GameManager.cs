229191029ee1b17fc4b2cb181e04ceeec6355ada
しゅう <44467870+shumm7@users.noreply.github.com>
2020-01-16 01:11:32 +0900

    商品ボタン実装中

--- Assets/Script/Order/GameManager.cs
+++ Assets/Script/Order/GameManager.cs
@@ -8,16 +8,19 @@ public class GameManager : MonoBehaviour
 {
     public Text TimeUI;
     public GameObject Tabs;
+    public GameObject OrderButton;
     private DataLoader.List productList;
+    public int TabMode = 0;
 
     void Start()
     {
         productList = GetComponent<DataLoader>().LoadList();
-        Debug.Log(productList.CategoryName[1]);
         for(int i = 1; i<=4; i++)
         {
             Tabs.transform.Find(i.ToString()).transform.Find("Text").GetComponent<Text>().text = productList.CategoryName[i - 1];
+            Tabs.transform.Find(i.ToString()).gameObject.GetComponent<Button>().interactable = true;
         }
+        Tabs.transform.Find("1").gameObject.GetComponent<Button>().interactable = false;
     }
 
     // Update is called once per frame
@@ -37,5 +40,19 @@ public class GameManager : MonoBehaviour
         {
             return num.ToString();
         }
+    }
+
+    public void TabButtonClicked(int num)
+    {
+        TabMode = num;
+        for (int i = 0; i < 6; i++)
+        {
+            OrderButton.transform.Find((i + 1).ToString()).gameObject.GetComponent<OrderButton>().TabChanged(TabMode, i + 1);
+        }
+        for(int i=0; i<4; i++)
+        {
+            Tabs.transform.Find((i+1).ToString()).gameObject.GetComponent<Button>().interactable = true;
+        }
+        Tabs.transform.Find((num+1).ToString()).gameObject.GetComponent<Button>().interactable = false;
     }
 }
---
+++ Assets/Script/Order/OrderButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderButton : MonoBehaviour
+{
+    public GameObject Controller;
+    private int TabMode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TabMode = Controller.GetComponent<GameManager>().TabMode;
+    }
+
+    public void TabChanged(int TabMode, int num)
+    {
+        Debug.Log(num);
+    }
+
+    public void ButtonPressed(int num)
+    {
+
+    }
+}
